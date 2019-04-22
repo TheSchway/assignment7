5b956a6ae8be613bcca5d78bdf2b8e2c0192ed73
Daniel Aronson <daronson@u.rochester.edu>
2019-04-22 16:37:04 -0400

    php
    jdfiejp

---
+++ index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<title>Assignment 07: A Survey: Shrek</title>
+</head>
+
+<body>
+
+
+	<div>
+
+	<header>
+		<a href ="index.php">
+			<h1>Shrek</h1>
+		</a>
+	</header>
+
+	</div>
+
+
+	<nav>
+		<ul>
+			<li><a href = "index.php">Home</a></li>
+			<li><a href = "shrek1.php">Shrek 1</a></li>
+			<li><a href = "shrek2.php">Shrek 2</a></li>
+			<li><a href = "shrek3.php">Shrek 3</a></li>
+		</ul>
+	</nav>
+
+	<div>
+	<div>
+
+		<img src="images/shrek.jpg" alt="Shrek">
+
+
+	</div>
+	</div>
+
+	<div>
+
+		<main>
+			<div>
+				<h2>A Recap!</h2>
+				<p>Get some information on what happens during these movies!</p>
+			</div>
+			<div>
+				<h2>We need your Help!</h2>
+				<p>Help us figure out your favorite parts about Shrek!</p>
+			</div>
+		</main>
+	</div>
+
+	<div>
+
+		<ol>
+			<li>Click Here to Learn More About These Movies!</li>
+		</ol>
+		</a>
+
+	</div>
+
+	<div>Assignment 07: Shrek</div>
+
+	<script src="//code.jquery.com/jquery.js"></script>
+	<script src="js/menu-highlighter.js"></script>
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+	<script src="sss/sss.js"></script>
+	<script>$('.slider').sss();</script>
+
+</body>
+</html>
---
+++ shrek3.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Assignment 07: Shrek 3</title>
+</head>
+
+<body>
+
+
+	<div>
+
+	<header>
+		<a href ="index.php">
+			<h1>Shrek The Third</h1>
+		</a>
+	</header>
+
+	</div>
+
+
+	<nav>
+		<ul>
+			<li><a href = "index.php">Home</a></li>
+			<li><a href = "shrek1.php">Shrek 1</a></li>
+			<li><a href = "shrek2.php">Shrek 2</a></li>
+			<li><a href = "shrek3.php">Shrek 3</a></li>
+		</ul>
+	</nav>
+
+	<div>
+		<header>
+			<h1>Shrek The Third</h1>
+			<h3>2007</h3>
+		</header>
+	</div>
+
+
+	<h2>Plot</h2>
+
+		<p>Shrek and Princess Fiona are to succeed the dying King Harold. Shrek's attempts to serve as the Regent during the King's medical leave end in disaster; Shrek insists that an ogre as king is a bad idea and that there must be someone else to rule the kingdom. Before he dies, Harold tells Shrek of another heir: his nephew, Arthur "Artie" Pendragon. Meanwhile, Prince Charming vows to himself that he will become King of Far Far Away and avenge the death of his mother, the Fairy Godmother. Charming goes to the Poison Apple tavern and persuades fairy tale villains to fight for their "happily ever after".
+
+		Shrek, Donkey and Puss in Boots set out to retrieve Artie; as they are sailing away, Fiona reveals to Shrek that she is pregnant, much to Shrek's horror, as he believes he is not capable of raising children. The trio journey to Worcestershire Academy, an elite magical boarding school, where they discover Artie is a scrawny 16-year-old underachiever. At the school pep rally, Shrek tells Artie he will be king of Far Far Away. Artie is excited until Donkey and Puss inadvertently frighten him by discussing the responsibilities of being king. Artie tries taking control of the ship to go back to Worcestershire and as he and Shrek fight over the wheel, it breaks and the ship crashes on a remote island, where they encounter Artie's retired wizard teacher, Merlin.
+
+		Charming and other villains attack the castle, but Wolfie, Pinocchio, Gingy, the Three Little Pigs and the Blind Mice stall them long enough for Fiona and her mother Queen Lillian to escape along with Snow White, Rapunzel, Cinderella, Sleeping Beauty, and Doris the Ugly Stepsister. One of the Pigs accidentally blurts out that Shrek has gone to retrieve Arthur, and Prince Charming sends Captain Hook and his pirates to track down Shrek. The ladies are put in a tower after Rapunzel betrays them, having fallen in love with Charming.
+
+		Captain Hook and his pirates track Shrek and his friends to Merlin's island, where they attempt to capture Shrek. Shrek and Artie defeat the villains, and Hook mentions Charming and the takeover of Far Far Away. Shrek urges Artie to return to Worcestershire. Instead, Artie cons Merlin into using his magic to send them to Far Far Away. The spell causes Puss and Donkey to accidentally switch bodies. They find Pinocchio and learn that Charming plans to kill Shrek as part of a play. Charming's men arrive, but Artie tricks the knights into not taking them into custody and they break into the castle during rehearsals for the play. Caught in Charming's dressing room, the four are taken captive.
+
+		Charming prepares to kill Artie, believing he is the next king. To save Artie's life, Shrek tells Charming that Artie was a pawn to take his place as King of Far Far Away. Charming believes Shrek and allows Artie to leave. Donkey and Puss are imprisoned with Fiona and the ladies, where Fiona grows frustrated with their lack of initiative. Fiona's mother, Queen Lillian, smashes an opening in the stone wall of the prison with a headbutt. While the princesses launch a rescue mission for Shrek, Donkey and Puss free Gingy, Pinocchio, the wolf and pigs, and Dragon and Donkey's children. They encounter Artie; Puss and Donkey mollify him by explaining that Shrek lied to save Artie's life.
+
+		Charming stages a musical in front of the kingdom. Just as Charming is about to kill Shrek, Fiona, along with Puss, Donkey, the princesses and the fairy tale characters confront the villains. They lose, and Fiona is tied up. Artie shows up and convinces the villains that just because they are being treated like losers does not mean they must be losers. The villains agree to give up their evil ways. Charming refuses to listen, and lunges at Artie with his sword, but Shrek blocks the blow, so Charming attacks him instead. As Shrek pushes Charming aside, Dragon knocks over Rapunzel's tower, seemingly crushing him to death.
+
+		Artie is crowned king. While the kingdom celebrates, Merlin restores Puss and Donkey to their correct bodies. Shrek and Fiona return to the swamp, becoming the parents of ogre triplets, and coping with the help of Puss, Lillian, Donkey and Dragon.</p>
+
+
+	<h2>Music</h2>
+		<p>I NEED TO FILL THIS STILL</p>
+
+
+	<h2>Reception</h2>
+		<p>Shrek the Third opened in 4,122 North American theaters on May 18, 2007, grossing $38 million on its first day, the biggest opening day for an animated film at the time, for a total of $121.6 million in its first weekend, the best opening weekend ever for an animated film, and the second-highest opening for a 2007 film in the United States and Canada. It held the animated opening weekend record for nine years until it was surpassed by Finding Dory's $135.1 million debut. At the time, its opening weekend was the third-highest of all time in these regions.
+
+		Shrek the Third grossed $322.7 million in the United States, and $476.2 million overseas, bringing its cumulative total to $799 million. The film was the fourth-highest-grossing film worldwide of 2007, and the second-highest-grossing film in the United States and Canada that year. In addition, it was the highest-grossing animated film of 2007, and the third-highest-grossing animated film ever. The film sold an estimated 46,907,000 tickets in North America.
+
+		The film was released in the United Kingdom on June 29, 2007, and topped the country's box office for the next two weekends, before being dethroned by Harry Potter and the Order of the Phoenix.</p>
+
+
+	<script src="//code.jquery.com/jquery.js"></script>
+	<script src="js/menu-highlighter.js"></script>
+
+</body>
+</html>
