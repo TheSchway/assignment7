f74ccf5ef0c8c261a2bcdbeaa992ddd5edf60349
Joshua Dennis <jdenn11@u.rochester.edu>
2019-04-28 19:23:31 -0400

    Didn't really do much of anything

---
+++ survey.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Survey | Seoul | Assignment 7</title>
+</head>
+<body>
+	<label for="name">Name: </label>
+	<input type="text" name="name" id="name" placeholder="Enter your name...">
+
+	<label for="email">Email: </label>
+	<input type="email" name="email" id="email" placeholder="Enter your email...">
+
+	<label for="entry">Placeholder: </label>
+	<input type="text" name="entry" id="entry" placeholder="placeholder...">
+
+	<input type="submit" value="Submit">
+</body>
+</html>
