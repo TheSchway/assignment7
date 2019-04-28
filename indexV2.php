8a8ce108f9fe357ec8d2f1cb132875a19c05d21f
Eric Feirouz <efeirouz@u.rochester.edu>
2019-04-28 19:41:50 -0400

    new

---
+++ indexV2.php
@@ -0,0 +1,80 @@
+	<!DOCTYPE html>
+	<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Assignment 07: A Survey: Shrek</title>
+		<link rel="stylesheet" href="css/styles.css">
+		<link rel="stylesheet" href="css/navigation.css">
+
+	</head>
+
+
+
+
+	<body class>
+		<div class="bannerContainer">
+
+			<div class="banner1">
+				<header>
+					<a href="index.php">
+						<h1> Shrek</h1>
+					</a>
+
+				</header>
+			</div>
+
+
+			<div class="banner2">
+
+				<nav class="menu">
+
+					<ul>
+						<li><a href = "shrek3.php">Shrek 3</a></li>
+						<li><a href = "shrek2.php">Shrek 2</a></li>
+						<li><a href = "shrek1.php">Shrek 1</a></li>
+						<li><a href = "index.php">Home</a></li>
+					
+					</ul>
+
+				</nav>
+			</div>
+		</div>
+
+
+		<div>
+
+			<img src="images/shrek.jpg" alt="Shrek">
+
+
+		</div>
+
+
+
+		<main>
+			<div class="flex-container">
+				<div class="botbanner">
+					<h2>A Recap!</h2>
+					<p>Get some information on what happens during these movies!</p>
+
+
+
+					<h2>We Need Your Help!</h2>
+					<p> Click Here To Give Us Your Input on Shrek!</p>
+				</div>
+
+				<div class="vertical">
+					<ol>
+						<li>Click Here to Learn More About These Movies!</li>
+					</ol>
+				</a>
+
+			</div>
+		</div>
+
+	</main>
+	<footer class="foot">CSC 174: Advanced Front-end Web Design and Development</footer>
+	<script src="http://code.jquery.com/jquery.js"></script>
+	<script src="js/menu-highlighter.js"></script>
+
+</body>
+</html>
