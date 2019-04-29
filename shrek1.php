ed4ffe85134170714c42eb44a972ce439f0cf114
Eric Feirouz <efeirouz@u.rochester.edu>
2019-04-29 11:25:38 -0400

    new

--- shrek1.php
+++ shrek1.php
@@ -3,63 +3,99 @@
 <head>
 	<meta charset="utf-8">
 	<title>Assignment 07: Shrek 1</title>
+	<link rel="stylesheet" href="css/styles.css">
+	<link rel="stylesheet" href="css/navigation.css">
 </head>
 
 <body>
 
 
-	<div>
+	<div class="bannerContainer">
 
-	<header>
-		<a href ="index.php">
-			<h1>Shrek</h1>
-		</a>
-	</header>
+		<div class="banner1">
+			<header>
+				<a href="index.php">
+					<h1> SHREK</h1>
+				</a>
 
+			</header>
+		</div>
+
+
+		<div class="banner2">
+
+			<nav class="menu">
+
+				<ul>
+					<li><a href = "shrek3.php">Shrek 3</a></li>
+					<li><a href = "shrek2.php">Shrek 2</a></li>
+					<li><a href = "shrek1.php">Shrek 1</a></li>
+					<li><a href = "index.php">Home</a></li>
+					
+				</ul>
+
+			</nav>
+		</div>
 	</div>
 
 
-	<nav>
-		<ul>
-			<li><a href = "index.php">Home</a></li>
-			<li><a href = "shrek1.php">Shrek 1</a></li>
-			<li><a href = "shrek2.php">Shrek 2</a></li>
-			<li><a href = "shrek3.php">Shrek 3</a></li>
-		</ul>
-	</nav>
+	<div class="container">
 
-	<div>
-		<header>
-			<h1>Shrek</h1>
-			<h3>2001</h3>
-		</header>
+		<div>
+			<header>
+				<h1>Shrek</h1>
+				<h3>2001</h3>
+			</header>
+		</div>
+
+
+		<div class="flex-shrek">
+
+			<h2 class="flexh">Plot </h2>
+
+
+			<div class="content">
+				<p>Shrek, a mean and highly territorial green ogre who loves the solitude of his swamp, finds his life interrupted when countless fairytale creatures are exiled there by the fairytale-hating and vertically-challenged Lord Farquaad of Duloc. Angered, he decides to ask Farquaad to exile them elsewhere. Shrek brings along a talking Donkey, who is the only fairytale creature willing to guide him to Duloc.</p>
+
+
+				<p>Meanwhile, Farquaad tortures the Gingerbread Man for the location of the remaining fairytale creatures. His guards rush in with something he has been searching for: the Magic Mirror. He asks the Mirror if his kingdom is the fairest of them all but is told that he is not even a king, as he isn't descended from royalty. To become a king, he must marry a princess; Farquaad resolves to marry Princess Fiona, who is locked in a castle tower guarded by a dragon. Unwilling to perform the task himself, he organizes a tournament wherein the winner gets the "privilege" of rescuing Fiona for him. Shrek and Donkey arrive during the tournament, and ignorantly defeat Farquaad's knights. Farquaad proclaims them the champions, and compels them under threat of death to rescue Fiona, promising to move the fairytale creatures from Shrek's swamp if he succeeds.</p>
+
+				<p>Shrek and Donkey travel to the castle to find Fiona. They are noticed by Dragon, who corners Donkey. In desperation, he sweet-talks the beast, learning that it is female. Dragon falls in love with Donkey and carries him to her chambers. Meanwhile, Shrek finds Fiona, who is appalled at his lack of romanticism and surprised he had not slain Dragon. They leave after rescuing Donkey, and Fiona is thrilled to be rescued but is quickly disappointed when Shrek reveals he is an ogre. Despite her demands that Farquaad come get her in person, Shrek forcibly carries her as he ventures back to Duloc with Donkey. The three encounter Robin Hood on their way back, where it is revealed that Fiona is an expert martial artist. Shrek and Fiona find they have a lot in common and begin to fall in love.</p>
+
+				<p>Shrek interrupts the wedding before Farquaad can kiss Fiona and tells her that Farquaad is not her true love and is only marrying her to become king. The sun sets, which turns Fiona into an ogress in front of everyone, causing a surprised Shrek to fully understand what he overheard. Outraged, Farquaad orders Shrek executed and Fiona detained. Dragon bursts in alongside Donkey and devours Farquaad alive. Shrek and Fiona profess their love and share a kiss; Fiona's curse is lifted but she is surprised to see that she is still an ogress. They marry in the swamp and leave on their honeymoon.</p>
+			</div>
+
+		</div>
+
+		<div class="flex-shrek">
+
+			<h2 class="flexhmusic">Music</h2>
+
+
+			<div class="content">
+				<p>Shrek is the third DreamWorks animated film (and the only film in the Shrek series) to have Harry Gregson-Williams team up with John Powell to compose the score following Antz (1998) and Chicken Run (2000). John Powell was left out to compose scores for later Shrek films with Williams due to a conflict.[47] The score was recorded at Abbey Road Studios by Nick Wollage and Slamm Andrews, with the latter mixing it at Media Ventures and Patricia Sullivan-Fourstar handling mastering.</p>
+
+				<p>Shrek introduced a new element to give the film a unique feel. The film used pop music and other Oldies to make the story more forward. Covers of songs like "On the Road Again" and "Try a Little Tenderness" were integrated in the film's score. As the film was about to be completed, Katzenberg suggested to the filmmakers to redo the film's ending to "go out with a big laugh"; instead of ending the film with just a storybook closing over Shrek and Fiona as they ride off into the sunset, they decided to add a song "I'm a Believer" covered by Smash Mouth and show all the fairytale creatures in the film.</p>
+				
+
+			</div>
+
+		</div>
+
+		
+
+		<div class="flex-shrek">
+			<h2 class="flexhawards">Awards</h2>
+			<div class="content">
+				<p>Shrek opened on around 6,000 screens[95] across 3,587 theaters;[96] eleven of them showed the film digitally, made possible by the THX Division of Lucasfilm.[97] This was the first time that DreamWorks had shown one of its films digitally. The film earned $11.6 million on its first day and $42.3 million on its opening weekend, topping the box office for the weekend and averaging $11,805 from 3,587 theaters. In its second weekend, due to the Memorial Day Weekend holiday, the film gained 0.3 percent to $42.5 million and $55.2 million over the four-day weekend, resulting in an overall 30 percent gain. Despite this, the film finished in second place behind Pearl Harbor and had an average of $15,240 from expanding to 3,623 sites. In its third weekend, the film retreated 34 percent to $28.2 million for a $7,695 average from expanding to 3,661 theaters. The film closed on December 6, 2001, after grossing $267.7 million domestically, along with $216.7 million overseas, for a worldwide total of $484.4 million. Produced on a $60 million budget, the film was a huge box office smash and is the fourth highest-grossing film of 2001 behind Harry Potter and the Philosopher's Stone, The Lord of the Rings: The Fellowship of the Ring, and Monsters, Inc.. The film sold an estimated 47,290,600 tickets in North America.</p>
+
+				<p>Shrek became the highest-grossing animated film ever to be released in Australia, passing the mark set by The Lion King in 1994. In the United Kingdom, Shrek regained the top spot at the British box office after being beaten out the previous week by Lara Croft: Tomb Raider, earning a $20.3 million since its opening in the UK.</p>
+
+			</div>
+		</div>
 	</div>
 
-
-	<h2>Plot</h2>
-
-		<p>Shrek, a mean and highly territorial green ogre who loves the solitude of his swamp, finds his life interrupted when countless fairytale creatures are exiled there by the fairytale-hating and vertically-challenged Lord Farquaad of Duloc. Angered, he decides to ask Farquaad to exile them elsewhere. Shrek brings along a talking Donkey, who is the only fairytale creature willing to guide him to Duloc.</p>
-
-		<p>Meanwhile, Farquaad tortures the Gingerbread Man for the location of the remaining fairytale creatures. His guards rush in with something he has been searching for: the Magic Mirror. He asks the Mirror if his kingdom is the fairest of them all but is told that he is not even a king, as he isn't descended from royalty. To become a king, he must marry a princess; Farquaad resolves to marry Princess Fiona, who is locked in a castle tower guarded by a dragon. Unwilling to perform the task himself, he organizes a tournament wherein the winner gets the "privilege" of rescuing Fiona for him. Shrek and Donkey arrive during the tournament, and ignorantly defeat Farquaad's knights. Farquaad proclaims them the champions, and compels them under threat of death to rescue Fiona, promising to move the fairytale creatures from Shrek's swamp if he succeeds.</p>
-
-		<p>Shrek and Donkey travel to the castle to find Fiona. They are noticed by Dragon, who corners Donkey. In desperation, he sweet-talks the beast, learning that it is female. Dragon falls in love with Donkey and carries him to her chambers. Meanwhile, Shrek finds Fiona, who is appalled at his lack of romanticism and surprised he had not slain Dragon. They leave after rescuing Donkey, and Fiona is thrilled to be rescued but is quickly disappointed when Shrek reveals he is an ogre. Despite her demands that Farquaad come get her in person, Shrek forcibly carries her as he ventures back to Duloc with Donkey. The three encounter Robin Hood on their way back, where it is revealed that Fiona is an expert martial artist. Shrek and Fiona find they have a lot in common and begin to fall in love.</p>
-
-		<p>Shrek interrupts the wedding before Farquaad can kiss Fiona and tells her that Farquaad is not her true love and is only marrying her to become king. The sun sets, which turns Fiona into an ogress in front of everyone, causing a surprised Shrek to fully understand what he overheard. Outraged, Farquaad orders Shrek executed and Fiona detained. Dragon bursts in alongside Donkey and devours Farquaad alive. Shrek and Fiona profess their love and share a kiss; Fiona's curse is lifted but she is surprised to see that she is still an ogress. They marry in the swamp and leave on their honeymoon.</p>
-
-
-	<h2>Music</h2>
-		<p>Shrek is the third DreamWorks animated film (and the only film in the Shrek series) to have Harry Gregson-Williams team up with John Powell to compose the score following Antz (1998) and Chicken Run (2000). John Powell was left out to compose scores for later Shrek films with Williams due to a conflict.[47] The score was recorded at Abbey Road Studios by Nick Wollage and Slamm Andrews, with the latter mixing it at Media Ventures and Patricia Sullivan-Fourstar handling mastering.</p>
-
-		<p>Shrek introduced a new element to give the film a unique feel. The film used pop music and other Oldies to make the story more forward. Covers of songs like "On the Road Again" and "Try a Little Tenderness" were integrated in the film's score. As the film was about to be completed, Katzenberg suggested to the filmmakers to redo the film's ending to "go out with a big laugh"; instead of ending the film with just a storybook closing over Shrek and Fiona as they ride off into the sunset, they decided to add a song "I'm a Believer" covered by Smash Mouth and show all the fairytale creatures in the film.</p>
-
-
-
-	<h2>Reception</h2>
-		<p>Shrek opened on around 6,000 screens[95] across 3,587 theaters;[96] eleven of them showed the film digitally, made possible by the THX Division of Lucasfilm.[97] This was the first time that DreamWorks had shown one of its films digitally. The film earned $11.6 million on its first day and $42.3 million on its opening weekend, topping the box office for the weekend and averaging $11,805 from 3,587 theaters. In its second weekend, due to the Memorial Day Weekend holiday, the film gained 0.3 percent to $42.5 million and $55.2 million over the four-day weekend, resulting in an overall 30 percent gain. Despite this, the film finished in second place behind Pearl Harbor and had an average of $15,240 from expanding to 3,623 sites. In its third weekend, the film retreated 34 percent to $28.2 million for a $7,695 average from expanding to 3,661 theaters. The film closed on December 6, 2001, after grossing $267.7 million domestically, along with $216.7 million overseas, for a worldwide total of $484.4 million. Produced on a $60 million budget, the film was a huge box office smash and is the fourth highest-grossing film of 2001 behind Harry Potter and the Philosopher's Stone, The Lord of the Rings: The Fellowship of the Ring, and Monsters, Inc.. The film sold an estimated 47,290,600 tickets in North America.</p>
-
-		<p>Shrek became the highest-grossing animated film ever to be released in Australia, passing the mark set by The Lion King in 1994. In the United Kingdom, Shrek regained the top spot at the British box office after being beaten out the previous week by Lara Croft: Tomb Raider, earning a $20.3 million since its opening in the UK.</p>
-
-
+	</div>
 	<script src="//code.jquery.com/jquery.js"></script>
 	<script src="js/menu-highlighter.js"></script>
 
