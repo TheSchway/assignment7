8ec4059451a7fa15a49e8ba454e3b479d680ebf8
Daniel Aronson <daronson@u.rochester.edu>
2019-04-29 10:42:57 -0400

    survey updatr
    survey update

--- survey.php
+++ survey.php
@@ -10,8 +10,20 @@
 	<label for="email">Email: </label>
 	<input type="email" name="email" id="email" placeholder="Enter your email...">
 
-	<label for="entry">Placeholder: </label>
-	<input type="text" name="entry" id="entry" placeholder="placeholder...">
+	<label for="entry">Location: </label>
+	<input type="text" name="location" id="location" placeholder="Enter the city you're from...">
+
+	<label for="entry">Which is your favorite Shrek movie? </label>
+	<input type="text" name="favmovie" id="favmovie" placeholder="Type here...">
+
+	<label for="entry">Who is your favorite Shrek character? </label>
+	<input type="text" name="favchar" id="favchar" placeholder="Type here...">
+
+	<label for="entry">Which song from the Shrek movies is your favorite? </label>
+	<input type="text" name="favsong" id="favsong" placeholder="Type here...">
+
+	<label for="entry">Who is the meanest Shrek villain? </label>
+	<input type="text" name="mean" id="mean" placeholder="Type here...">
 
 	<input type="submit" value="Submit">
 </body>
