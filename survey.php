7f3f0887bc590daa19d4d885f5a01d24f4f26876
Eric Feirouz <efeirouz@u.rochester.edu>
2019-05-01 09:50:13 -0400

    final push

--- survey.php
+++ survey.php
@@ -98,5 +98,8 @@
 	<a href = "login.php">Administrators Only: Click Here To Login To Your Account</a>
 
 	</footer>
+
+	<script src="//code.jquery.com/jquery.js"></script>
+	<script src="js/menu-highlighter.js"></script>
 	</body>
 	</html>
